21c6cf2630dbcc86c13acc9c737f1e1512df07cc
Lourival Linard <l.linard@gmail.com>
2026-05-11 23:40:29 -0300

    feat: add request dto documentation

--- src/main/java/com/auth/web/dto/request/RegisterRequest.java
+++ src/main/java/com/auth/web/dto/request/RegisterRequest.java
@@ -1,5 +1,6 @@
 package com.auth.web.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
@@ -7,10 +8,12 @@ import lombok.Data;
 @Data
 public class RegisterRequest {
 
+    @Schema(example = "user@email.com", description = "User email")
     @Email
     @NotBlank
     private String email;
 
+    @Schema(example = "StrongPassword123", description = "User password")
     @NotBlank
     private String password;
 }
